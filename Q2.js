91ff202ab1df6b07b1e0c68400fb993b5892c0e7
Suchitra Guduru <72724857+suchitraguduru@users.noreply.github.com>
2021-04-08 20:48:02 +0530

    Delete Q2.js

--- Q2.js
+++
@@ -1,8 +0,0 @@
-/*2. Write the pseudocode & a program to calculate the simple interest based on the inputs(amount, rate, time)
- provided by the user.*/
-
- var amount = 5000;
- var rate = 4;
- var time = 2;
- var si = (amount*rate*time)/100;
- console.log(si);
